f1e3e6d77f6c628fe6d6cab6968957b384f3e554
Mykel <Mykellaguirre@gmail.com>
2022-03-01 18:05:55 -0800

    first commit

---
+++ resources/views/error.blade.php
@@ -0,0 +1,21 @@
+@extends('adminlte::page')
+
+@section('title', 'Error')
+
+@section('content_header')
+    <h1>Error</h1>
+@stop
+
+@section('content')
+    <p>404 error please search another page.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
+
+
