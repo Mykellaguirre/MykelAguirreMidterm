1193b4367e62e270298fb57fa23e34e2d3599349
Mykel <Mykellaguirre@gmail.com>
2022-03-12 14:57:26 -0800

    second commit

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,27 @@ Route::fallback(function () {
     return view('what-ever');
 });
 
+Route::get('events-feed/' , function () {
+	return ' [
+    {
+      "title": "CSE4500 Class",
+      "start": "2022-02-23T17:30:00",
+      "end": "2022-02-23T18:45:00"
+    },
+    {
+      "title": "CSE4500 Class",
+      "start": "2022-02-28T17:30:00",
+      "end": "2022-02-28T18:45:00"
+    },   
+  ]';
+});
 
+Route::get('/db-test', function () {
+    try {         
+         echo \DB::connection()->getDatabaseName();     
+    } catch (\Exception $e) {
+          echo 'None';
+    }
+});
 
 
