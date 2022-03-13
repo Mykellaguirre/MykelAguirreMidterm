6090ea91a5462e047e5d06c49f20a4614af12fc8
Mykel <Mykellaguirre@gmail.com>
2022-03-12 19:10:18 -0800

    calendar 4

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -31,7 +31,7 @@ $( document ).ready(function() {
     var calendar = new FullCalendar.Calendar($('#calendar')[0], {
       initialView: 'dayGridMonth',
       events: '/events-feed'
-      
+      return json_encode( compact('events')['events'] );
     });
     calendar.render();
 })
