6663bd3aff01904eb583753b6b5e019b738c020a
Mykel <Mykellaguirre@gmail.com>
2022-03-16 02:09:42 -0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -55,40 +55,9 @@ Route::fallback(function () {
     return view('what-ever');
 });
 
-Route::get('events-feed/' , function () {
-	return ' [
-    {
-      "title": "CSE4500 Class",
-      "start": "2022-02-23T17:30:00",
-      "end": "2022-02-23T18:45:00"
-    },
-    {
-      "title": "CSE4500 Class",
-      "start": "2022-02-28T17:30:00",
-      "end": "2022-02-28T18:45:00"
-    },   
-  ]';
-});
-
-Route::get('/db-test', function () {
-    try {         
-         echo \DB::connection()->getDatabaseName();     
-    } catch (\Exception $e) {
-          echo 'None';
-    }
-});
-
-Route::get('/db-migrate', function () {
-    Artisan::call('migrate');
-    echo Artisan::output();
-});
-
-Route::get(uri:'index',action:'CalendarController@index')->name(name:'allEvent');
 
 
 
-Route::resource('/todos', TodoController::class);
-Route::resource('/calendar', CalendarController::class);
 
 
 
