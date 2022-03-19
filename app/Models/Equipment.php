3f3d1659072a999a854b6829af52178c72fd8125
Mykel <Mykellaguirre@gmail.com>
2022-03-18 18:56:54 -0700

    first commit

--- app/Models/Equipment.php
+++ app/Models/Equipment.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Equipment extends Model
 {
     use HasFactory;
+    protected $fillable = ['name','note_id','price','manufacturer_id','type_id','invoice_Num','specs'];
 
     function manufacturer() {
         return $this->belongsTo(Manufacturer::class);// manufacturer
