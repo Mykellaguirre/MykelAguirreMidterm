c47bd643823645e93a13af320f63bdea584d5550
Mykel <Mykellaguirre@gmail.com>
2022-03-23 18:36:54 -0700

    first commit

--- database/migrations/2022_03_17_042257_create_note_table.php
+++ database/migrations/2022_03_17_042257_create_note_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
-            $table->mediumText('note');
+            $table->string('note');
             $table->string('contact');
             $table->timestamps();
         });
