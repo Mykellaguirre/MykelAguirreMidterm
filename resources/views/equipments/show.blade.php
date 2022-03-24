3ac2d2f0b96be0dbc74e396647592f3e9e134335
Mykel <Mykellaguirre@gmail.com>
2022-03-23 17:26:16 -0700

    first commit

--- resources/views/equipments/show.blade.php
+++ resources/views/equipments/show.blade.php
@@ -11,15 +11,15 @@
    <div class = "col-md-12">
      <div class="card card-primary">
        <div class = "card-body">
-         <b>Name</b> {|| $equipment->name;||} |
-          <b>Manufacturer</b> {|| $equipment->manufacturer->man_Name||} |
-            <b>Price</b> $equipment->price} |
-            <b>Category</b>  $equipment->type->category} |
-            <b>Processor</b>  $equipment->processor} |
-            <b>Screen Size</b> $equipment->screen_size}|
-            <b>Battery Life</b>  $equipment->battery} |
-            <b>Invoice#</b> $equipment->invoice_id } |
-            <b>User</b>  $equipment->note->employee} |
+         <b>Name</b> {{ $equipment->name}} |
+          <b>Manufacturer</b> {{ $equipment->manufacturer->man_Name}} |
+            <b>Price</b> {{$equipment->price}} |
+            <b>Category</b>  {{$equipment->type->category}} |
+            <b>Processor</b>  {{$equipment->processor}} |
+            <b>Screen Size</b> {{$equipment->screen_size}}|
+            <b>Battery Life</b>  {{$equipment->battery}} |
+            <b>Invoice#</b> {{$equipment->invoice_id }} |
+            <b>User</b>  {{$equipment->note->employee}} |
 
  
   
