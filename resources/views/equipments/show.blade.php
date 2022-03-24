81c8c6f9f5cf09450af5c6414c3f6630b22b946f
Mykel <Mykellaguirre@gmail.com>
2022-03-23 17:48:41 -0700

    first commit

--- resources/views/equipments/show.blade.php
+++ resources/views/equipments/show.blade.php
@@ -17,12 +17,12 @@
          <b>Name</b> {{ $equipment->name}} |
           <b>Manufacturer</b> <a href="{{ route('manufacturers.show',['manufacturer'=>$equipment->manufacturer->id]) }}"    >   {{$equipment->manufacturer->man_Name}}</a>
             <b>Price</b> {{$equipment->price}} |
-            <b>Category</b>  {{$equipment->type->category}} |
+            <b>Category</b> <a href="{{ route('types.show',['type'=>$equipment->type->id]) }}" > {{$equipment->type->category}} |</a>
             <b>Processor</b>  {{$equipment->processor}} |
             <b>Screen Size</b> {{$equipment->screen_size}}|
             <b>Battery Life</b>  {{$equipment->battery}} |
             <b>Invoice#</b> {{$equipment->invoice_id }} |
-            <b>User</b>  {{$equipment->note->employee}} |
+            <b>User</b>  <a href="{{ route('note.show',['note'=>$equipment->note->id]) }}">{{$equipment->note->employee}} |</a>
 
  
   
